5ba1f8b6bcaecb44e20b7bb0374f2802064289ef
Esterjudith <judy.l.hanson@gmail.com>
2022-06-29 09:13:59 -0700

    solution022 completed

---
+++ solution022.js
@@ -0,0 +1,17 @@
+// Return the number (cReturn the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// ount) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) { 
+    let vowelCount = 0
+    vowelCount = str.match(/[a,e,i,o,u]/g)
+    return vowelCount ? vowelCount.length : 0
+  }
